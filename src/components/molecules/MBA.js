b6bd3b4b2550d29741efb67a72f8bc858a08b916
Vchuks <64593598+Vchuks@users.noreply.github.com>
2023-04-28 18:50:20 +0100

    corrected cert button

--- src/components/molecules/MBA.js
+++ src/components/molecules/MBA.js
@@ -9,12 +9,12 @@ const MBA = () => {
   return (
     <div className="cert">
       <h2 className="text-center my-3 my-lg-5 text-black" data-aos="zoom-in-up" data-aos-duration="2000">Sage Mini MBA</h2>
-      <div  className="row align-items-center gap-3">
+      <div  className="row align-items-center gap-3 gap-lg-0">
         <div className="col-sm-12 col-md-12 col-lg-6 p-0" data-aos="fade-right" data-aos-duration="2000">
           <h2 className="fs-1 std size">Stand Out With</h2>
           <h2 className="fs-1 std2 size">Sage Mini MBA</h2>
           <Text className="py-3 mbacol" children="Sage Mini MBA is one the most advanced program of its kind globally. The Mini MBA focuses gives you the same academic value of a full-time MBA in a streamlined and self-paced environment and for a fraction of the cost. Sage Mini MBA is backed internationally and adheres to global standards for advanced business studies."/>
-          <Button className="btn header-btn" children="Get Started"/>
+          <Button className="btn cert-btn" children="Get Started"/>
         </div>
         <div className="col-sm-12 col-md-12 col-lg-6 p-0" data-aos="fade-up" data-aos-duration="2000">
           <Image className="mbalogo" src={sagemini} />
